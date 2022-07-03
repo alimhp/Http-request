127dad0673a3e6556397c58c6afa6f0dcb711563
alimhp <alimohamadipourr@gmail.com>
2022-07-03 02:06:54 -0700

    get data with aysnc-await

--- src/Container/Discussion/Discussion.jsx
+++ src/Container/Discussion/Discussion.jsx
@@ -9,10 +9,17 @@ const Discussion = () => {
   const [comments, setComments] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/comments")
-      .then((response) => setComments(response.data.slice(0, 4)))
-      .catch((error) => console.log(error));
+    const getComments = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+        setComments(data.slice(0, 4));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getComments();
   }, []);
 
   return (
