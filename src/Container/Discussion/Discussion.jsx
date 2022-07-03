c225b1ea4d5cc6fb312cf6753d21f29d50ab1bf2
alimhp <alimohamadipourr@gmail.com>
2022-07-03 01:15:31 -0700

    get data with then.catch

--- src/Container/Discussion/Discussion.jsx
+++ src/Container/Discussion/Discussion.jsx
@@ -2,13 +2,29 @@ import Comment from "../../component/Comment/Comment";
 import FullComment from "../../component/Full-comment/FullComment";
 import AddComment from "../../component/Add-Comment/AddComment";
 import "./Discussion.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Discussion = () => {
+  const [comments, setComments] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => setComments(response.data.slice(0, 4)))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <main>
       <section>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments ? (
+          comments.map((c) => (
+            <Comment name={c.name} email={c.email} key={c.id} />
+          ))
+        ) : (
+          <p>loading...</p>
+        )}
       </section>
       <section>
         <FullComment />
